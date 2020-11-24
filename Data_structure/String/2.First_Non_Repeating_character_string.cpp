4064b368655ce9a83679d08a05cb8f67da7b384f
Ataullah Masum <masumruet1@gmail.com>
2020-11-24 15:11:20 +0600

    Find first non repeating character

--- Data_structure/String/2.First_Non_Repeating_character_string.cpp
+++ Data_structure/String/2.First_Non_Repeating_character_string.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+char Find_Duplicate_Character_In_A_String(string str)
+{
+    map<char, int> Map;
+    for(int i=0;i<str.size();i++)
+    {
+        Map[str[i]]++;
+    }
+    for(auto it=Map.begin(); it!=Map.end();it++)
+    {
+        if(it->second==1)
+        {
+            return (it->first);
+        }
+    }
+}
+int main()
+{
+    string str;
+    cout<<"Enter The String: ";
+    cin>>str;
+    cout<<"\nDisplay first Non Repeating Character in String: "<<endl;
+    cout<<Find_Duplicate_Character_In_A_String(str);
+    return 0;
+}
 
