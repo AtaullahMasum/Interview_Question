133c5fc44922c89bd11ad0cbfab0f1337d9b550f
Ataullah Masum <masumruet1@gmail.com>
2020-11-25 17:55:54 +0600

    Bubble SOrt

--- Data_structure/Sort/BubbleSort.cpp
+++ Data_structure/Sort/BubbleSort.cpp
@@ -1,1 +1,38 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void BubbleSort(int arr[], int n)
+{
+    cout<<"Before Sorting: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0; j<(n-i-1); j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<"\nAfter Sorting: ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter The Number Of Element: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    BubbleSort(arr, n);
+    return 0;
+}
